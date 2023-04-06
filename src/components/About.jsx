7ca8449784daa47bf7223027f6cfa4953d907409
Lautaro Mielgo <lautimielgo@gmail.com>
2023-04-05 21:04:40 -0300

    Rick And Morty

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+const About = () =>{
+      return(
+            <div>
+                  <h1>Work in progress...</h1>
+                  <p>Lautaro esta trabajando para solucionarlo...</p>
+                  
+            </div>
+      )
+}
+
+export default About
